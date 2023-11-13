7c6cc3f86e917838f2d0d6a1d2f3c0bfae1a9945
jthill57 <jthill57@gmail.com>
2023-11-13 16:37:00 -0500

    Testing config setup

--- config/index.js
+++ config/index.js
@@ -1,19 +1,20 @@
-import { createConfig as createDefaultConfig } from './sites/default';
-import { createConfig as createSHBCConfig } from './sites/shbc';
-import { createConfig as createSFBCConfig } from './sites/sfbc';
-import { createConfig as createLBCDurhamConfig } from './sites/lbc_durham';
+
+import { getValidSubdomain } from '@/lib/helpers';
+import { defineConfig } from './defineConfig';
+import { ENABLED_CONFIGS, SITES } from '@/lib/constants';
+
+const SITE_CONFIGS = {
+  [SITES.SHBC]: require('./sites/shbc.js'),
+  [SITES.SFBC]: require('./sites/sfbc.js'),
+  [SITES.LBC_DURHAM]: require('./sites/lbc_durham.js'),
+}
 
 export const appConfig = getConfig();
 
 function getConfig() {
-  switch (process.env.SITE) {
-    case 'SHBC':
-      return createSHBCConfig();
-    case 'SFBC':
-      return createSFBCConfig();
-    case 'LBC_DURHAM':
-      return createLBCDurhamConfig();
-    default:
-      return createDefaultConfig();
-  }
+  const subdomain = getValidSubdomain();
+  const siteConfig = SITE_CONFIGS[subdomain];
+
+  if (ENABLED_CONFIGS.includes(subdomain)) return defineConfig(siteConfig);
+  return defineConfig();
 }
--- config/sites/default.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from '../defineConfig';
-
-export function createConfig() {
-  return defineConfig();
-}
