f78221d3a4af0af7136a95b9e508579d80eaa679
jthill57 <jthill57@gmail.com>
2023-11-13 16:50:58 -0500

    One last fix

--- config/index.js
+++ config/index.js
@@ -3,13 +3,13 @@ import { defineConfig } from './defineConfig';
 import { ENABLED_CONFIGS, SITES } from '@/lib/constants';
 
 const SITE_CONFIGS = {
-  [SITES.SHBC]: require('./sites/shbc.js'),
-  [SITES.SFBC]: require('./sites/sfbc.js'),
-  [SITES.LBC_DURHAM]: require('./sites/lbc_durham.js'),
+  [SITES.SHBC]: require('./sites/shbc.js').default,
+  [SITES.SFBC]: require('./sites/sfbc.js').default,
+  [SITES.LBC_DURHAM]: require('./sites/lbc_durham.js').default,
 }
 
 export function getAppConfig(host) {
-  const subdomain = 'sfbc';
+  const subdomain = getValidSubdomain(host);
   const siteConfig = SITE_CONFIGS[subdomain];
 
   if (ENABLED_CONFIGS.includes(subdomain)) return defineConfig(siteConfig);
