f8cd9d17590450e1898a1f4ac38d760e5a7b7f72
jthill57 <jthill57@gmail.com>
2023-11-14 12:21:38 -0500

    Try this one more time

--- components/Header.js
+++ components/Header.js
@@ -2,18 +2,16 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { Dialog } from '@headlessui/react'
+import { Popover } from '@headlessui/react'
 import { useTranslation } from 'react-i18next';
 import { Bars3Icon, FilmIcon, MusicalNoteIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
-import { useToggle } from '@/lib/hooks';
 import { useApp } from '@/context/AppContext';
 import { LanguageSelector } from './LanguageSelector';
 
 export default function Header() {
   const app = useApp();
   const { t } = useTranslation();
-  const [mobileMenuOpen, toggleMenuOpen] = useToggle(false);
 
   return (
     <header className="bg-white w-full sticky top-0 z-10">
@@ -36,20 +34,49 @@ export default function Header() {
           </Link>
         </div>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={mobileMenuOpen ? () => {} : () => toggleMenuOpen()}
-          >
-            <span className="sr-only">Open main menu</span>
-            {mobileMenuOpen ? <XMarkIcon className="x-6 w-6" aria-hidden="true" /> : <Bars3Icon className="h-6 w-6" aria-hidden="true" />}
-          </button>
+          <Popover className="relative">
+            {({ open }) => (
+              <>
+                <Popover.Button className="outline-none p-2 -m-2">
+                  {open ? <XMarkIcon className="h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="h-6 w-6" aria-hidden="true" />}
+                </Popover.Button>
+                {open && (
+                  <div className="fixed top-20 left-2 right-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50 flex flex-col -space-y-2
+                  lg:origin-top-right lg:absolute lg:top-auto lg:bottom-auto lg:left-auto lg:right-0 lg:mt-4 lg:-mr-4 overflow-auto lg:max-h-[calc(100vh-196px)] lg:w-screen lg:max-w-[800px]">
+                    <div className="flow-root px-6 py-2 divide-y">
+                      <h1 className="mt-4 mb-4 font-semibold text-gray-800 text-base">
+                        {t('more_content')} <span className="text-xs font-normal text-gray-600 ml-1">({t('english_only')})</span>
+                      </h1>
+                      <div className="space-y-2 py-2">
+                      <Link
+                          href="/videos"
+                          onClick={() => setMobileMenuOpen(false)}
+                          className="-mx-6 flex items-center gap-3 rounded-lg px-6 py-2 text-base font-semibold leading-7 text-sky-700 hover:bg-gray-50 outline-none"
+                        >
+                          <FilmIcon className="h-6" />
+                          {t('videos')}
+                        </Link>
+                        <Link
+                          href="/hymns"
+                          onClick={() => setMobileMenuOpen(false)}
+                          className="-mx-6 flex items-center gap-3 rounded-lg px-6 py-2 text-base font-semibold leading-7 text-sky-700 hover:bg-gray-50 outline-none hidden"
+                        >
+                          <MusicalNoteIcon className="h-6" />
+                          {t('hymns')}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+          </Popover>
         </div>
         <div className="hidden lg:flex flex-1 justify-end -mx-2">
           <LanguageSelector hideNameOnMobile />
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={toggleMenuOpen}>
+      {/* <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={toggleMenuOpen}>
         <Dialog.Panel className="fixed top-20 left-2 right-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50 flex flex-col -space-y-2
               lg:origin-top-right lg:absolute lg:top-auto lg:bottom-auto lg:left-auto lg:right-0 lg:mt-4 lg:-mr-4 overflow-auto lg:max-h-[calc(100vh-196px)] lg:w-screen lg:max-w-[800px]">
           <div className="flow-root px-6 py-2 divide-y">
@@ -76,7 +103,7 @@ export default function Header() {
             </div>
           </div>
         </Dialog.Panel>
-      </Dialog>
+      </Dialog> */}
     </header>
   );
 }
