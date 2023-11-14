db7a31f260f16dc563fbf99aabef6483479d8488
jthill57 <jthill57@gmail.com>
2023-11-14 12:02:54 -0500

    Test

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ export default function Header() {
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={toggleMenuOpen}
+            onClick={mobileMenuOpen ? () => {} : () => toggleMenuOpen()}
           >
             <span className="sr-only">Open main menu</span>
             {mobileMenuOpen ? <XMarkIcon className="x-6 w-6" aria-hidden="true" /> : <Bars3Icon className="h-6 w-6" aria-hidden="true" />}
