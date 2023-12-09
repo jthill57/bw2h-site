dc33c67432d299ec5656b932c046ad10113bba0b
Josh Hill <jthill57@gmail.com>
2023-12-09 11:54:17 -0500

    Update middleware to properly address any lang

--- middleware.js
+++ middleware.js
@@ -1,7 +1,49 @@
+import { NextResponse } from 'next/server';
 import { i18nRouter } from 'next-i18n-router';
 import i18nConfig from './i18nConfig';
 
 export function middleware(request) {
+  const {
+    locales,
+    defaultLocale,
+    prefixDefault = false,
+    basePath = ''
+  } = i18nConfig;
+
+  const pathname = request.nextUrl.pathname;
+  const basePathTrailingSlash = basePath.endsWith('/');
+
+  const pathLocale = locales.find(
+    locale => new RegExp(`^/(${locale}-[a-zA-Z]{2})($|/)`).test(pathname)
+  );
+
+  if (pathLocale) {
+    let pathWithoutLocale = pathname.slice(`/${defaultLocale}`.length) || '/';
+
+    if (pathWithoutLocale.includes('-')){
+      pathWithoutLocale = 'en';
+    }
+
+    if (basePathTrailingSlash) {
+      pathWithoutLocale = pathWithoutLocale.slice(1);
+    }
+
+    if (request.nextUrl.search) {
+      pathWithoutLocale += request.nextUrl.search;
+    }
+
+    // If /default, redirect to /
+    if (!prefixDefault && pathLocale === defaultLocale) {
+      return NextResponse.redirect(
+        new URL(`${basePath}${pathWithoutLocale}`, request.url)
+      );
+    } else {
+      return NextResponse.redirect(
+        new URL(`${basePath}${pathLocale}`, request.url)
+      );
+    }
+  }
+
   return i18nRouter(request, i18nConfig);
 }
 
